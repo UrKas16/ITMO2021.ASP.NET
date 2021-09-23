e698938f633447b19cf83aeafd28c39c3459f218
Yurii <kasyurvik568@gmail.com>
2021-09-23 22:40:12 +0300

    ASP.NET.MVC Labs_02_exp_1 Start

--- ITMO2021.ASP.NET.MVC/Controllers/HomeController.cs
+++ ITMO2021.ASP.NET.MVC/Controllers/HomeController.cs
@@ -15,10 +15,41 @@ namespace ITMO2021.ASP.NET.MVC.Controllers
         //    return View();
         //}
 
-        public string Index(string hel)
+        public string Index()
         {
-            string Greeting = ModelClass.ModelHello() + ", " + hel;
-            return Greeting;
+            //string Greeting = ModelClass.ModelHello() + ", " + hel;
+            //return Greeting;
+
+            string res = ExeEnum();
+            return res;
         }
+
+        public string ExeEnum()
+        {
+            AccountType goldAccount;
+            AccountType platinumAccount;
+
+            goldAccount = AccountType.Cheking;
+            platinumAccount = AccountType.Deposit;
+
+            string res1 = String.Format("Тип банковского счета {0}", goldAccount);
+            string res2 = String.Format("Тип банковского счета {0}", platinumAccount);
+
+            string res = res1 + "<p>" + res2;
+            return res;
+        }
+
+        public string ExeStruct()
+        {
+            BankAccount goldBankAccount;
+
+            goldBankAccount.accType = AccountType.Cheking;
+            goldBankAccount.accBal = (decimal)3200.00;
+            goldBankAccount.accNo = 123;
+
+            string res = String.Format("Номер счета {0}, баланс {1}, тип {2}", goldBankAccount.accNo, goldBankAccount.accBal, goldBankAccount.accType);
+            return res;
+        }
+
     }
 }
---
+++ ITMO2021.ASP.NET.MVC/Models/StudyCsharp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ITMO2021.ASP.NET.MVC.Models
+{
+    public enum AccountType
+    {
+        Cheking,
+        Deposit
+    }
+
+    public class StudyCsharp
+    {
+
+    }
+
+    public struct BankAccount
+    {
+        public long accNo;
+        public decimal accBal;
+        public AccountType accType;
+    }
+}
